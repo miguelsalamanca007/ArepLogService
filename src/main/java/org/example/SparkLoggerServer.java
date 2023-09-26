95d09f1cd7b9fff3c4a4edd538d2261d216e52dd
unknown <miguel.salamanca@labinfo.is.escuelaing.edu.co>
2023-09-26 15:40:23 -0500

    First Commit

---
+++ src/main/java/org/example/SparkLoggerServer.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static spark.Spark.port;
+import static spark.Spark.*;
+
+public class SparkLoggerServer {
+
+    public static void main(String... args) {
+        port(getPort());
+        get("logString", (req, res) -> {
+            String value = req.queryParams("value");
+            return "value";
+        });
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4568;
+    }
+}
